ec0aa68bb7ca8f42b569acbf8a55b4b1220aa57f
kdsgood12 <kdsgood12@daum.net>
2020-01-29 23:03:26 +0900

    app-create

---
+++ js-timer.js
@@ -0,0 +1,17 @@
+const jstime = document.querySelector('.js-time'),
+    jstime_h1 = jstime.querySelector('h1');
+
+function getTime(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    jstime_h1.innerHTML = `${hours<10?`0${hours}`:hours}:${minutes<10?`0${minutes}`:minutes}:${seconds<10?`0${seconds}`:seconds}`
+}
+
+function init(){
+    getTime();
+    setInterval(getTime,1000);
+}
+
+init();
